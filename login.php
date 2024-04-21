c8787c7e3608c714ec445bdd0eae3e06fd8aad9e
Ethan Gamble <z1918687@turing.cs.niu.edu>
2024-04-20 19:28:28 -0500

    updated login php to have the forms + brief descriptions on what it should do

--- login.php
+++ login.php
@@ -1,3 +1,49 @@
 <html>
-<p>hello world</p>
+<!-- THIS IS WHERE LOGIN BOXES WILL BE -->
+<head>
+<title>Quote Site Login</title>
+</head>
+
+
+<body>
+  <h1>Welcome to Quotes Daily</h1>
+</body>
+
+<form>
+
+  <label for='UserID'>User ID: </label>
+  <input type='text' id='UserID' name='UserID'>
+
+  <br/>
+  <br/>
+
+  <label for='Password'>Password: </label>
+  <input type='text' id='password' name='Password'>
+
+  <br/>
+  <br/>
+
+  <!-- This button hopefully is for the id + password -->
+  <button type='submit'>SUBMIT</button>
+
+</form>
+
+<br/>
+<br/>
+<a href=''> CLICK HERE TO REGISTER AS NEW CUSTOMER </a>
+
+<br/>
+
+<?php
+echo 'this page should check 2 things: 1) username 2)password';
+
+echo '<br/>';
+
+echo 'check against a table in our DB';
+
+echo '<br/>';
+
+echo 'then i want to include something that lets a new customer sign up';
+?>
+
 </html>
