7d1a33a382d9165f5cc278020b727082e514d9e8
Magali Gutierrez <z1915336@turing.cs.niu.edu>
2024-04-29 15:28:50 -0500

    Add ons file for Sales Asociate.

---
+++ save.php
@@ -0,0 +1,6 @@
+color: <style> body { background-color: #Ffe5ec;} </style>
+
+number of employees:
+<?php $total_customers = $result->num_rows;?>
+<p>show total number of customers: <?PHP echo $total_customers; ?></p>
+  
