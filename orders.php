d06e7c637c5b33108cc154f4b9176a9041f47c12
Ashley Tiu <Z1956079@students.niu.edu>
2024-05-01 18:15:41 -0500

    added customer to orders page

--- orders.php
+++ orders.php
@@ -6,7 +6,7 @@
    include "header.php";
    include "connection.php"; // include database connection from connection.php
 
-   //get customer table data from Legacy DB
+   //get order table data from the database
    $query = "SELECT * FROM OrderInfo LEFT JOIN Quotes on OrderInfo.QuoteID = Quotes.QuoteID";
 
    try {
@@ -17,18 +17,46 @@
      die("<p> Query failed: {$e->getMessage()}</p>\n");
    }
 
+   //get customer table data from the legacy database
+   $query = "SELECT * FROM customers";
+
+   try {
+      $statement = $legacyPdo->prepare($query);
+      $statement->execute();
+      $customers = $statement->fetchAll(PDO::FETCH_ASSOC);
+   } catch(PDOException $e) {
+      die("<p> Customer Query failed: {$e->getMessage()}</p>");
+   }
+
    //make order table
    echo "<table border='1'>";
-   echo "<tr><th>ID</th><th>Status</th><th>Item</th><th>Order Qty</th><th>Price</th><th>Discount</th></tr>";
+   echo "<tr>";
+   echo "<th style='width: 100px'>Order ID</th>";
+   echo "<th style='width: 150px'>Customer</th>";
+   echo "<th style='width: 100px'>Status</th>";
+   echo "<th style='width: 150px'>Item</th>";
+   echo "<th style='width: 100px'>Order Qty</th>";
+   echo "<th style='width: 100px'>Price</th>";
+   echo "<th style='width: 100px'>Discount</th>";
+   echo "</tr>";
    foreach ($rows as $row) {
-       echo "<tr class='detail-row'>";
-       echo "<td>".$row['PurchaseID']."</td>";
-       echo "<td>".$row['OrderStatus']."</td>";
-       echo "<td>".$row['ItemNum']."</td>";
-       echo "<td>".$row['OrderQuant']."</td>";
-       echo "<td>".$row['Prices']."</td>";
-       echo "<td>".$row['Discounts']."</td>";
-       echo "</tr>";
+      // find the customer for this order
+      $customer = null;
+      foreach($customers as $cust) {
+         if($cust->id == $row->CustomerID) {
+            $customer = $cust;
+            break;
+         }
+      }
+      echo "<tr class='detail-row'>";
+      echo "<td>".$row['PurchaseID']."</td>";
+      echo "<td>".$customer['name']."</td>";
+      echo "<td>".$row['OrderStatus']."</td>";
+      echo "<td>".$row['ItemNum']."</td>";
+      echo "<td>".$row['OrderQuant']."</td>";
+      echo "<td>$".$row['Prices']."</td>";
+      echo "<td>$".$row['Discounts']."</td>";
+      echo "</tr>";
    }
 
    echo "</table>";
